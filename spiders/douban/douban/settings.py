67ad2636d601c0a0345a16e5f9beb1a8e32f5c10
evaliu <1872863416@qq.com>
2019-11-26 15:39:13 +0800

    Optimization function

--- spiders/douban/douban/settings.py
+++ spiders/douban/douban/settings.py
@@ -14,12 +14,9 @@ BOT_NAME = 'douban'
 SPIDER_MODULES = ['douban.spiders']
 NEWSPIDER_MODULE = 'douban.spiders'
 
-# TODO 设置默认请求头
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
              'Chrome/78.0.3904.108 Safari/537.36'
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'douban (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- spiders/douban/douban/spiders/douban_spider.py
+++ spiders/douban/douban/spiders/douban_spider.py
@@ -2,69 +2,42 @@
 import scrapy
 import re
 import json
-import requests
 
-from urllib.parse import unquote
-from bs4 import BeautifulSoup
 from ..items import DoubanItem
+from urllib.parse import unquote
+from scrapy.http import Request
 
 
 class DoubanSpiderSpider(scrapy.Spider):
     name = 'douban_spider'
     allowed_domains = ['https://www.douban.com/']
     start_urls = ['http://https://www.douban.com/j/search/']
-    USER_AGENT = {
-        'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                      'Chrome/78.0.3904.108 Safari/537.36'
-    }
 
     def start_requests(self):
         search_info = 'python'
         start_value = 5
         search_url = 'https://www.douban.com/j/search?q={}&start={}&cat=1015'.format(search_info, start_value)
-        yield scrapy.Request(search_url, headers=self.USER_AGENT)
-
-    @staticmethod
-    def get_further_url(response):
-        for i in json.loads(response.body)['items']:
-            url_info = re.search('a href="(.+?)"', i)
-            if url_info:
-                yield url_info.group(1)
-            else:
-                yield None
-
-    def further_request(self, url):
-        resp = requests.get(url=url, headers=self.USER_AGENT)
-
-        item = DoubanItem()
-        soup = BeautifulSoup(resp.text, 'lxml')
-
-        note_id = re.search('note/(.+?)/&amp', unquote(url)).group(1)
-        content = soup.find('div', id='note-{}'.format(note_id))
-        item['title'] = content.find('div', class_='note-header note-header-container').find('h1').text
-        item['article'] = content.find('div', id='note_{}_full'.format(note_id)).text
-        item['url'] = url
-        return item
+        return [Request(url=search_url, callback=self.parse)]
 
     def parse(self, response):
-        for each_url in self.get_further_url(response):
-            if each_url:
-                items = self.further_request(each_url)
-                yield items
+        for i in json.loads(response.body)['items']:
+            url_info = re.search('a href="(.+?)"', i)
 
-        # loops = 0
-        # while True:
-        #     loops += 1
-        #     item = DoubanItem()
-        #     for each_url in self.get_next_url(response):
-        #         item['url'] = each_url
-        #         yield item
-        #         yield scrapy.Request(url=each_url, headers=self.headers, callback=self.next_request)
-        #
-        #     if loops <= 2:
-        #         page_num = re.search(r'start=(\d+)', response.url).group(1)
-        #         page_num = 'start=' + str(int(page_num) + 20)
-        #         next_url = re.sub(r'start=\d+', page_num, response.url)
-        #         yield scrapy.Request(url=next_url, headers=self.headers, callback=self.parse)
-        #     else:
-        #         break
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/78.0.3904.108 Safari/537.36'
+            }
+            yield Request(url=url_info.group(1), headers=headers, callback=self.parse_each_url)
+
+        # page_num = re.search(r'start=(\d+)', response.url).group(1)
+        # page_num = 'start=' + str(int(page_num) + 20)
+        # next_url = re.sub(r'start=\d+', page_num, response.url)
+        # yield Request(url=next_url, callback=self.parse)
+
+    def parse_each_url(self, response):
+        note_id = re.search('note/(.+?)/&amp', unquote(response.url)).group(1)
+        item = DoubanItem()
+        item['title'] = response.xpath('//*[@id="note-{}"]/div[1]/h1/text()'.format(note_id)).extract_first()
+        item['article'] = response.xpath('//*[@id="note_{}_full"]//text()'.format(note_id)).extract()
+        item['url'] = response.url
+        yield item
