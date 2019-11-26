443dc99f1d65289de6d36053184d19e1f4878e25
evaliu <1872863416@qq.com>
2019-11-26 18:12:44 +0800

    增加注释

--- spiders/example/example/middlewares.py
+++ spiders/example/example/middlewares.py
@@ -25,29 +25,7 @@ class RandomUserAgentMiddleware(object):
         ]
 
     def process_request(self, request, spider):
-        # TODO 在发送请求到 Download 之前调用此方法，可以修改User-Agent、处理重定向、设置代理、失败重试、设置Cookies等功能
         request.headers['User-Agent'] = random.choice(self.user_agents)
-
-    def process_response(self, request, response, spider):
-        # TODO 在收到 Response 之后调用此方法，可以处理响应结果再发送给 Spider 解析
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object  # TODO 返回一个全新的 Request 放到调度队列，等待被调度
-        # - or raise IgnoreRequest
-        return response
-
-    def process_exception(self, request, exception, spider):
-        # TODO 当 Downloader 或 process_request() 方法抛出异常时，会调用此方法
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain  # TODO 返回一个全新的 Request 放到调度队列，等待被调度
-        pass
 
 
 class ExampleSpiderMiddleware(object):
@@ -63,6 +41,7 @@ class ExampleSpiderMiddleware(object):
         return s
 
     def process_spider_input(self, response, spider):
+        # TODO 当 Response 被 Spider MiddleWare 处理时，会调用此方法
         # Called for each response that goes through the spider
         # middleware and into the spider.
 
@@ -70,6 +49,7 @@ class ExampleSpiderMiddleware(object):
         return None
 
     def process_spider_output(self, response, result, spider):
+        # TODO 当 Spider 处理 Response 返回结果时，会调用此方法
         # Called with the results returned from the Spider, after
         # it has processed the response.
 
@@ -86,6 +66,7 @@ class ExampleSpiderMiddleware(object):
         pass
 
     def process_start_requests(self, start_requests, spider):
+        # TODO 以 Spider 启动的 Request 为参数被调用，执行的过程类似 process_spider_output()，必须返回 Request
         # Called with the start requests of the spider, and works
         # similarly to the process_spider_output() method, except
         # that it doesn’t have a response associated.
@@ -111,34 +92,37 @@ class ExampleDownloaderMiddleware(object):
         return s
 
     def process_request(self, request, spider):
+        # TODO 在发送请求到 Download 之前调用此方法，可以修改User-Agent、处理重定向、设置代理、失败重试、设置Cookies等功能
         # Called for each request that goes through the downloader
         # middleware.
 
         # Must either:
         # - return None: continue processing this request
         # - or return a Response object
-        # - or return a Request object
+        # - or return a Request object  # TODO 返回一个全新的 Request 放到调度队列，等待被调度
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
         return None
 
     def process_response(self, request, response, spider):
+        # TODO 在发送 Response 响应结果到 Spider 解析之前调用此方法，可以修改响应结果等功能
         # Called with the response returned from the downloader.
 
         # Must either;
         # - return a Response object
-        # - return a Request object
+        # - return a Request object  # TODO 返回一个全新的 Request 放到调度队列，等待被调度
         # - or raise IgnoreRequest
         return response
 
     def process_exception(self, request, exception, spider):
+        # TODO 当 Downloader 或 process_request() 方法抛出异常时，会调用此方法
         # Called when a download handler or a process_request()
         # (from other downloader middleware) raises an exception.
 
         # Must either:
         # - return None: continue processing this exception
         # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain  # TODO 返回一个全新的 Request 放到调度队列，等待被调度
         pass
 
     def spider_opened(self, spider):
