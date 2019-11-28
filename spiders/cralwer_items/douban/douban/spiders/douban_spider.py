f336c143ab727025e6c4b23bd021cf9a94a2e08c
evaliu <1872863416@qq.com>
2019-11-28 09:54:02 +0800

    Optimization function

--- spiders/cralwer_items/douban/douban/spiders/douban_spider.py
+++ spiders/cralwer_items/douban/douban/spiders/douban_spider.py
@@ -12,13 +12,17 @@ class DoubanSpiderSpider(scrapy.Spider):
     name = 'douban_spider'
     start_urls = ['https://www.douban.com/j/search?']
 
+    ARTICLE_COUNTS = 0  # 文章数量总数
+    LIMIT = 50  # 文章数量获取限制
+    SEARCH = 'python'  # 要搜索的关键字填入这里
+
     def start_requests(self):
         """
         构造初始请求
         :return:
         """
         params = {
-            'q': 'python',  # 要搜索的信息填入这里
+            'q': self.SEARCH,
             'start': 5,  # 偏移参数（每次增加20）
             'cat': 1015
         }
@@ -32,15 +36,24 @@ class DoubanSpiderSpider(scrapy.Spider):
         :param response:
         :return:
         """
-        for index, value in enumerate(json.loads(response.body)['items']):
-            after_url = unquote(re.search('url=(http.+?)&', value).group(1))
-            self.logger.info('Capture the url({}): {}'.format(index + 1, after_url))
-            yield Request(url=after_url, callback=self.parse)
+        if json.loads(response.body).get('items', None):
+            for index, value in enumerate(json.loads(response.body)['items']):
+                self.ARTICLE_COUNTS += 1
+                if self.ARTICLE_COUNTS > self.LIMIT:
+                    break
 
-        # page_num = re.search(r'start=(\d+)', response.url).group(1)
-        # page_num = 'start=' + str(int(page_num) + 20)
-        # next_url = re.sub(r'start=\d+', page_num, response.url)
-        # yield Request(url=next_url, callback=self.parse)
+                after_url = unquote(re.search('url=(http.+?)&', value).group(1))
+                yield Request(url=after_url, callback=self.parse)
+        else:
+            self.logger.warning('No data was found under this url ({}), continue to crawl'.format(response.url))
+
+        self.logger.info('ARTICLE_COUNTS is {}, LIMIT is {}'.format(self.ARTICLE_COUNTS, self.LIMIT))
+        if self.ARTICLE_COUNTS < self.LIMIT:
+            page_num = re.search(r'start=(\d+)', response.url).group(1)
+            page_num = 'start=' + str(int(page_num) + 20)
+            next_url = re.sub(r'start=\d+', page_num, response.url)
+            self.logger.info('Next url: {}'.format(next_url))
+            yield Request(url=next_url, callback=self.get_each_url)
 
     def parse(self, response):
         """
