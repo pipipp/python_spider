74df005f74c598528221455e9df51082a85c1b27
evaliu <1872863416@qq.com>
2019-12-01 14:21:14 +0800

    优化彼岸壁纸爬虫

--- spiders/public_crawlers/public_crawlers/pipelines.py
+++ spiders/public_crawlers/public_crawlers/pipelines.py
@@ -6,6 +6,8 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
 
+from scrapy.exceptions import DropItem
+
 
 class TextPipeline(object):
     """
@@ -13,12 +15,15 @@ class TextPipeline(object):
     """
     def process_item(self, item, spider):
         """
-        删除多余的空白行
+        丢弃空值，删除多余的空白行
         :param item:
         :param spider:
         :return:
         """
         for key, value in item.items():
+            if value == 'Not found':
+                return DropItem('Discard empty values')
+
             if isinstance(value, str):
                 item[key] = value.strip()
             elif isinstance(value, (tuple, list)):
