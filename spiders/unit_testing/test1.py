3d6797b618760ad6fcb3aefa11806af132cca2dc
Evanliuu <1872863416@qq.com>
2019-12-06 20:52:46 +0800

    优化单元测试代码

--- spiders/unit_testing/test1.py
+++ spiders/unit_testing/test1.py
@@ -1,5 +1,7 @@
 import json
 
+from urllib.parse import unquote
+
 
 def read_json_data(file_name='json_file.json'):
     """
@@ -10,10 +12,14 @@ def read_json_data(file_name='json_file.json'):
     with open(file_name, 'r', encoding='utf-8') as rf:
         # 将JSON对象转换为字典
         json_dict = json.loads(rf.read())
-        return json_dict
+
+    print('Load json file:\n{}'.format(json_dict))
+    print('Json data length: {}'.format(len(json_dict)))
+    return json_dict
 
 
 if __name__ == '__main__':
-    result = read_json_data(file_name=r'C:\Evan\my_programs\scrapy_code\spiders\public_crawlers\bian_wallpaper.json')
-    print(result)
-    print(len(result))
+    # result = read_json_data(file_name=r'C:\Evan\my_programs\scrapy_code\spiders\public_crawlers\bian_wallpaper.json')
+
+    url = 'https://www.lagou.com/jobs/list_%E7%88%AC%E8%99%AB%E5%BC%80%E5%8F%91%E5%B7%A5%E7%A8%8B%E5%B8%88'
+    print(unquote(url))
