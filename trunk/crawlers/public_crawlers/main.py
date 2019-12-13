ffdd8fd86cb668e98f11c80e1c1b3aef8b8da766
evaliu <1872863416@qq.com>
2019-12-13 13:51:57 +0800

    添加拉钩网爬虫搜索职位

--- trunk/crawlers/public_crawlers/main.py
+++ trunk/crawlers/public_crawlers/main.py
@@ -6,4 +6,11 @@ from scrapy import cmdline
 # cmdline.execute("scrapy crawl bian_wallpaper -o bian_wallpaper.json".split())
 
 # TODO 运行lagou爬虫
-cmdline.execute("scrapy crawl lagou -o lagou.json".split())
+cmdline.execute("scrapy crawl lagou -o spider.json".split())
+# cmdline.execute("scrapy crawl lagou -o ai.json".split())
+# cmdline.execute("scrapy crawl lagou -o big_data.json".split())
+# cmdline.execute("scrapy crawl lagou -o web_development.json".split())
+# cmdline.execute("scrapy crawl lagou -o background_development.json".split())
+# cmdline.execute("scrapy crawl lagou -o automated_testing.json".split())
+# cmdline.execute("scrapy crawl lagou -o software_test.json".split())
+# cmdline.execute("scrapy crawl lagou -o hardware_test.json".split())
