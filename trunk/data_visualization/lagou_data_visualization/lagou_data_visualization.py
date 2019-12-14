8f7c82e2ce91474b1728a2f34165c84d1a1af2d9
Evanliuu <1872863416@qq.com>
2019-12-14 17:44:48 +0800

    优化拉勾网数据可视化脚本

--- trunk/data_visualization/lagou_data_visualization/lagou_data_visualization.py
+++ trunk/data_visualization/lagou_data_visualization/lagou_data_visualization.py
@@ -30,18 +30,28 @@ def plot(title, data_list=[], x_label=(), y_label=()):
     plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
     plt.rcParams['axes.unicode_minus'] = False
 
+    item_range = range(len(data_list[0]))
+
     plt.title(title)  # 添加标题
-    plt.bar(range(len(data_list)), data_list, align='center', color='steelblue', alpha=0.8)  # 绘柱形图
+    plt.bar(item_range, data_list[0], align='center',
+            alpha=0.8, width=0.4)  # 绘柱形图
+    # 向右移动0.4
+    plt.bar([i + 0.4 for i in item_range], data_list[1], align='center',
+            color='y', alpha=0.8, width=0.4)
 
     plt.xlabel(x_label[0])  # 添加X轴标签
-    plt.xticks(range(len(data_list)), x_label[1])  # 添加X轴刻度标签
+    plt.xticks([i + 0.2 for i in item_range], x_label[1])  # 添加X轴刻度标签
 
     plt.ylabel(y_label[0])  # 添加Y轴标签
     plt.ylim(y_label[1])  # 设置Y轴的刻度范围
 
-    # 为每个条形图添加数值标签
-    for x, y in enumerate(data_list):
+    # 为第一个条形图添加数值标签
+    for x, y in enumerate(data_list[0]):
         plt.text(x, y+100, '%s' % round(y, 1), ha='center')
+
+    # 为第二个条形图添加数值标签
+    for x, y in enumerate(data_list[1]):
+        plt.text(x + 0.4, y + 100, '%s' % round(y, 1), ha='center')
 
     plt.show()  # 显示图形
     # plt.savefig('./123.jpg')  # 保存图片
@@ -49,7 +59,7 @@ def plot(title, data_list=[], x_label=(), y_label=()):
 
 def analysis(data, json_file_name):
     """
-
+    分析职位薪资
     :param data: JSON数据
     :param json_file_name: JSON文件名
     :return:
@@ -63,7 +73,7 @@ def analysis(data, json_file_name):
         ('ai', ('人工智能', '智能|机器学习|区块链')),
         ('big_data', ('大数据', '大数据')),
         ('web_development', ('Web开发', 'web')),
-        ('background_development', ('后台开发', '后台')),
+        ('background_development', ('后台开发', '后台|后端')),
         ('spider', ('爬虫开发', '爬虫')),
         ('automated_testing', ('自动化测试', '自动化|测试')),
         ('software_test', ('软件测试', '软件|软件测试')),
@@ -71,16 +81,16 @@ def analysis(data, json_file_name):
     ]
     for info in data:
         salary = re.sub('k|K', '000', info['salary'], re.I).split('-')  # 把"k"转换为"000"，并分解为一个元组（最低薪，最高薪）
-        # calculate = str(salary[0])  # 计算最低薪资
-        # calculate = str(salary[1])  # 计算最高薪资
-        calculate = str((int(salary[0]) + int(salary[1])) // len(salary))  # 计算薪资平均值
+        minimum = int(salary[0])  # 最低薪资
+        maximum = int(salary[1])  # 最高薪资
+        # calculate = int((int(salary[0]) + int(salary[1])) // len(salary))  # 计算薪资平均值
 
         for mapper in mapping:
             if mapper[0] in str(json_file_name).lower():
                 condition = re.search(mapper[1][1], str(info['position_name']).lower())
                 if condition:
-                    matched.append(calculate)
-                    position_name = mapper[1][0]
+                    matched.append([minimum, maximum])  # 保存薪资最小值和最大值
+                    position_name = mapper[1][0]  # 保存职位名称
                     break
                 else:
                     continue
@@ -90,13 +100,14 @@ def analysis(data, json_file_name):
     # if not_matched:
     #     print('Not matched length: {}, data: {}'.format(len(not_matched), not_matched))
     if matched:
-        return [position_name + '\n({})'.format(len(matched)), eval('+'.join(matched))//len(matched)]
+        return [position_name + '\n({})'.format(len(matched)),
+                sum(i[0] for i in matched)//len(matched), sum(i[1] for i in matched)//len(matched)]
     else:
         return None
 
 
 def main():
-    root_path = r'C:\Evan\my_programs\scrapy_code\trunk\data_visualization\lagou_data_visualization\json_file\\'
+    root_path = r'C:\Evan\my_program\moon\trunk\data_visualization\lagou_data_visualization\json_file\\'
     result = []
     for file in os.listdir(root_path):
         # print('Read the file: {}'.format(file))
@@ -107,10 +118,10 @@ def main():
 
     if result:
         print('Final result length: {}\ndata: {}'.format(len(result), result))
-        plot(title='软件岗位平均薪资大比拼',
-             data_list=[item[1] for item in result],
+        plot(title='2019软件岗位薪资大比拼',
+             data_list=[[item[1] for item in result], [item[2] for item in result]],
              x_label=('深圳地区', [item[0] for item in result]),
-             y_label=('Salary', [5000, 35000]))
+             y_label=('Salary', [5000, 40000]))
 
 
 if __name__ == '__main__':
