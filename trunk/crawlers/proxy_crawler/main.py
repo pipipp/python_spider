d111f8a85ae866bd9e6221fbf11bf217f56a7c01
Evanliuu <1872863416@qq.com>
2020-02-18 15:50:09 +0800

    优化爬虫代理程序

--- trunk/crawlers/proxy_crawler/main.py
+++ trunk/crawlers/proxy_crawler/main.py
@@ -27,8 +27,8 @@ class ProxyHandle(object):
         :return:
         """
         threads = []
-        logger.info('开始多线程爬取西拉网站代理IP(抓取页数:{}, 线程池:{})...'.format(proxy_spider_settings['MAX_PAGE'],
-                                                                 proxy_spider_settings['THREAD_POOL_MAX']))
+        logger.info('开始多线程爬取西拉网站代理IP(抓取页数:{}, 线程池:{})...'
+                    .format(proxy_spider_settings['MAX_PAGE'], proxy_spider_settings['THREAD_POOL_MAX']))
         for page in range(1, proxy_spider_settings['MAX_PAGE'] + 1):
             t = threading.Thread(target=self.proxy_spider.get_xila_proxy_ip, args=(page,))
             threads.append(t)
@@ -38,19 +38,21 @@ class ProxyHandle(object):
 
         for thread in threads:
             thread.join()
-        logger.info('西拉网站代理IP爬取完毕\n')
+        logger.info('西拉网站代理IP爬取完毕，共爬取{}个代理IP\n'
+                    .format(len([i for i in self.proxy_spider.all_proxy_ip_table.find()])))
 
-    def verify_proxy_ip(self):
+    def verify_proxy_ip(self, delete_invalid_proxy=False):
         """
         多线程验证代理IP的有效性
+        :param delete_invalid_proxy: [bool] - 如果设置为True，则删除无效的代理
         :return:
         """
         threads = []
         all_proxy = [i for i in self.proxy_check.all_proxy_ip_table.find()]
-        logger.info('开始多线程验证所有代理IP的有效性(所有代理数量:{}, 线程池:{})...'.format(len(all_proxy),
-                                                                     proxy_check_settings['THREAD_POOL_MAX']))
+        logger.info('开始多线程验证所有代理IP的有效性(所有代理数量:{}, 线程池:{})...'
+                    .format(len(all_proxy), proxy_check_settings['THREAD_POOL_MAX']))
         for proxy_info in all_proxy:
-            t = threading.Thread(target=self.proxy_check.verify_proxy_ip, args=(proxy_info, ))
+            t = threading.Thread(target=self.proxy_check.verify_proxy_ip, args=(proxy_info, delete_invalid_proxy))
             threads.append(t)
 
         for thread in threads:
@@ -58,13 +60,14 @@ class ProxyHandle(object):
 
         for thread in threads:
             thread.join()
-        logger.info('所有代理IP验证完毕\n')
+        logger.info('所有代理IP验证完毕，共{}个有效代理IP\n'
+                    .format(len([i for i in self.proxy_check.valid_proxy_ip_table.find()])))
 
 
 def main():
     proxy_handle = ProxyHandle()
     proxy_handle.crawl_xila_proxy()  # 爬取西拉免费代理网站
-    proxy_handle.verify_proxy_ip()  # 验证所有代理IP的有效性
+    proxy_handle.verify_proxy_ip(delete_invalid_proxy=False)  # 验证所有代理IP的有效性
 
 
 if __name__ == '__main__':
